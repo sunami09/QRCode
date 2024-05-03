03e369f0ba12b3b870a1fb343f479cd134c24f5a
Sunami Dasgupta <sunamidasgupta@gmail.com>
2024-05-02 21:53:22 -0700

    MVP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { useRef, useState } from 'react';
+import QRCode from 'qrcode.react';
 import './App.css';
 
 function App() {
+
+  const nameRef = useRef(null);
+  const phoneRef = useRef(null);
+  const studentIDRef = useRef(null);
+  const [qrValue, setQrValue] = useState('');
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+    const data = `Name: ${nameRef.current.value}, Phone: ${phoneRef.current.value}, Student ID: ${studentIDRef.current.value}`;
+    setQrValue(data); 
+    console.log(data);
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='banner'>
+        <h1>Campus SignUP</h1>
+      </div>
+      <div className='form'>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Name: 
+            <input type="text" name="username" placeholder='Your full name' ref={nameRef}/>
+          </label>
+          <label>
+            Phone Number: 
+            <input type="number" name="phone" placeholder='XXX-XXX-XXXX' ref={phoneRef}/>
+          </label>
+          <label>
+            Student ID: 
+            <input type="number" name="studentID" placeholder='Your Student ID' ref={studentIDRef}/>
+          </label>
+            <button type="submit">Submit</button>
+        </form>
+        <div className='qr-code'>
+          {qrValue && <QRCode value={qrValue} />}  
+      </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
